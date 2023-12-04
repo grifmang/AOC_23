844c59076fd57ed9d5ed3778481d8b6b513cda4b
PythonGuy <grifmang@gmail.com>
2023-12-04 18:55:12 -0500

    Update input_file.py

--- input_file.py
+++ input_file.py
@@ -4,7 +4,7 @@ requests.packages.urllib3.disable_warnings()
 class GetInput():
 
     def __init__(self):
-        self.headers = '53616c7465645f5f3c86879490310204bc85dcc251d4185f3429885cd79de28ab76e39227d873ffc4c076ce255981a445d631b975f73d7ac2cd7b57fd914c69f'
+        self.headers = ''
     
     def get_input(self, day):
         headers = {'session': self.headers}
